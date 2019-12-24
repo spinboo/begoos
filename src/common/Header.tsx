9fdf47b08aa99b82dd563d1e82182f0a41fdb99a
dan <dpcrespo@gmail.com>
2019-12-24 20:12:20 +0100

    Added Header with styles

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,13 +1,53 @@
 import React from "react";
 import { Link } from "@reach/router";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const Header = () => {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      backgroundColor: "#212121"
+    },
+    toolBar: {
+      backgroundColor: "#212121"
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    link: {
+      color: "#fff",
+      textDecoration: "none"
+    }
+  })
+);
+
+export default function Header() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Link to="/">Begoos</Link>
-      <Link to="/about">Sobre Nosotros</Link>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolBar} variant="dense">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            <Link className={classes.link} to="/">
+              Begoos
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
-};
-
-export default Header;
+}
